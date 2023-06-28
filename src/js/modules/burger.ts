dccb08d708ea7e7a153ff0d46a8762025e871041
ananastasiap <anestheziia@gmail.com>
2023-06-28 11:35:50 +0300

    refactor: creak BREAKPOINT for reuse

--- src/js/modules/burger.ts
+++ src/js/modules/burger.ts
@@ -1,6 +1,7 @@
 export const burger = (burgerSelector: string, menuSelector: string) => {
   const menu: HTMLElement | null = document.querySelector(menuSelector);
   const burger: HTMLElement | null = document.querySelector(burgerSelector);
+  const BREAKPOINT = 992;
 
   if (!menu || !burger) {
     return;
@@ -9,12 +10,12 @@ export const burger = (burgerSelector: string, menuSelector: string) => {
   menu.style.display = 'none';
 
   burger.addEventListener('click', () => {
-    menu.style.display = (menu.style.display === 'none' && window.screen.availWidth < 993) ?
+    menu.style.display = (menu.style.display === 'none' && window.screen.availWidth < BREAKPOINT) ?
                          'block' : 'none';
   });
 
   window.addEventListener('resize', () => {
-    if(window.screen.availHeight > 992) {
+    if(window.screen.availHeight > BREAKPOINT) {
       menu.style.display = 'none';
     }
   });
